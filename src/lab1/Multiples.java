da4dd0bfdc45e923af80dcf67dcad31e0d7f1a5f
Atamert <atamert.cam@mail.utoronto.ca>
2025-09-09 17:17:12 -0400

    fixed order of a b n

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,12 +2,12 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int count = multiples(3, 5, 1000);
+        int count = multiples(1000, 3, 5);
 
         System.out.println(count);
     }
 
-    public static int multiples(int a, int b, int n) {
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++){
             if (i % a == 0 || i % b == 0){
